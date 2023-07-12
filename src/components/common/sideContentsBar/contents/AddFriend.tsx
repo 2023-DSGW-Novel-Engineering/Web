bb5ab2480ab22e6a22cbcfd9f4eb395b5b594a82
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-13 08:08:44 +0900

    ADD :: Connect Server

--- src/components/common/sideContentsBar/contents/AddFriend.tsx
+++ src/components/common/sideContentsBar/contents/AddFriend.tsx
@@ -2,10 +2,17 @@ import React, { useEffect, useState } from "react";
 import * as S from "./AddFriend.style";
 import ConnectPapago from "../../../../util/ConnectPapago";
 import AddIcon from "../../../../asset/Add.svg";
+import { Api } from "../../../../util/Api";
 
 const AddFriend = () => {
   const [addFriend, setAddFriend] = useState<boolean>(false);
   const [placeholder, setPlaceholder] = useState<string>("");
+  const userToken: string | null = localStorage.getItem("Token");
+  const AddFriendServer = () => {
+    Api.post("/api/addfriend", {
+      headers: { Authorization: `Bearer ${userToken}}` },
+    });
+  };
 
   useEffect(() => {
     ConnectPapago(setPlaceholder, "친구의 아이디를 입력해주세요.");
@@ -18,7 +25,9 @@ const AddFriend = () => {
             <div>
               <S.TitleAddFriends>친구 추가{"(Add Friends)"}</S.TitleAddFriends>
               <S.InputAddFriends type="text" placeholder={placeholder} />
-              <S.AddFriendBtn>친구추가</S.AddFriendBtn>
+              <S.AddFriendBtn onClick={AddFriendServer}>
+                친구추가
+              </S.AddFriendBtn>
             </div>
           </S.AddFriendContainer>
           <S.BackgroundComponents
