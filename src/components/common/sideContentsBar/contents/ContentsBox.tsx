7ea4d20e7b9562d72b43241a5a1ee459a6502e7a
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-13 02:05:44 +0900

    FIX :: Url Value

--- src/components/common/sideContentsBar/contents/ContentsBox.tsx
+++ src/components/common/sideContentsBar/contents/ContentsBox.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import * as S from "./ContentsBox.style";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 interface IContentsProps {
   context: string;
@@ -9,9 +9,10 @@ interface IContentsProps {
 
 const ContentsBox = (props: IContentsProps) => {
   const navigate = useNavigate();
+  const { username } = useParams();
   return (
     <S.MainContainer
-      onClick={() => navigate(`${props.url}/gg/${props.context}`)}
+      onClick={() => navigate(`${props.url}/${username}/${props.context}`)}
     >
       <div>{props.context}</div>
     </S.MainContainer>
