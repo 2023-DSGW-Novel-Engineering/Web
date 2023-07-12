3009e2d0305ec97829afb6057957b88a03fac5cf
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-12 17:21:27 +0900

    ADD :: SideContents Bar

---
+++ src/components/common/sideContentsBar/SideContentsBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./SideContentsbar.style";
+
+const SideContentsBar = () => {
+  return (
+    <S.MainSidebarContainer>
+      <div>SideContentsBar</div>
+    </S.MainSidebarContainer>
+  );
+};
+
+export default SideContentsBar;
---
+++ src/components/common/sideContentsBar/SideContentsbar.style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const MainSidebarContainer = styled.div`
+  padding: 0.5%;
+  width: 20rem;
+  height: 88%;
+  background-color: blue;
+  overflow: auto;
+`;
