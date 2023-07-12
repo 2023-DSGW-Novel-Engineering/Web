0473fc107711aab3b020d1e3d0a5b04a3cc777b3
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-12 14:13:33 +0900

    ADD :: Params Routing

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,6 +7,9 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<Auth />} />
+        <Route path="/userinfo/:username" />
+        <Route path="/userroom/:username" />
+        <Route path="/usersetting/:username" />
       </Routes>
     </BrowserRouter>
   );
