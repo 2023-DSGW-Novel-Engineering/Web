a401b6c020b994e5f8daeadbc94fc066b9be0f94
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-12 16:07:38 +0900

    ADD :: SideBar

---
+++ src/components/common/sidebar/ Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./Sidebar.style";
+
+const Sidebar = () => {
+  return (
+    <S.MainSidebarContainer>
+      <div>Sidebar</div>
+    </S.MainSidebarContainer>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/common/sidebar/Sidebar.style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const MainSidebarContainer = styled.div`
+  width: 5%;
+  height: 100%;
+  background-color: white;
+`;
