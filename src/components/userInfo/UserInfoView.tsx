d05c76e7ce3ea1d80f6fc2496e5dfa73e24b239a
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-12 23:21:05 +0900

    FIX :: Value

--- src/components/userInfo/UserInfoView.tsx
+++ src/components/userInfo/UserInfoView.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import Sidebar from "../common/sidebar/ Sidebar";
 import * as S from "../../styles/UserInfoView.style";
 import SideContentsBar from "../common/sideContentsBar/SideContentsBar";
+import { useParams } from "react-router-dom";
 
 const UserInfoView = () => {
+  const { friendname } = useParams();
   return (
     <S.MainContainer>
       <Sidebar />
-      <SideContentsBar />
-      <div>UserInfoView</div>
+      <SideContentsBar
+        title="친구"
+        contents={["ddf", "fdfdf", "dddd", "fdfdf", "fdfd", "ddd"]}
+        url="/userinfo"
+      />
+      <div>{friendname && <div>{friendname}</div>}</div>
     </S.MainContainer>
   );
 };
